7a2dd9158de243339c27dae04e70b543a566c94e
willsc <caron.wills@gmail.com>
2025-02-02 16:44:43 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "dev-eks-cluster"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Optional EC2 key pair name for SSH access to nodes (leave empty if not used)"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
